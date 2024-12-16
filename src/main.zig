dc1f55ac6faad94c50c9f6defa11a0fee297bdd1
Kristian Roth <kristian.roth95@gmail.com>
2024-12-16 12:11:54 +0200

    Solve day 15

--- src/main.zig
+++ src/main.zig
@@ -13,6 +13,7 @@ const aoc11 = @import("aoc11/aoc11.zig");
 const aoc12 = @import("aoc12/aoc12.zig");
 const aoc13 = @import("aoc13/aoc13.zig");
 const aoc14 = @import("aoc14/aoc14.zig");
+const aoc15 = @import("aoc15/aoc15.zig");
 
 pub fn main() !void {
     const args = std.process.argsAlloc(std.heap.page_allocator) catch return;
@@ -46,6 +47,7 @@ pub fn main() !void {
         12 => try aoc12.solve(),
         13 => try aoc13.solve(),
         14 => try aoc14.solve(),
+        15 => try aoc15.solve(),
         else => {
             std.debug.print("Day {d} not implemented\n", .{day});
             return;
